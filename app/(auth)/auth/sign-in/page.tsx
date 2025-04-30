1e131eb881e69c4f5b6a2f67db7220208913af19
moyasiginko <mrsiyam61@gmail.com>
2025-04-30 13:57:04 +0600

    fix page and setup suspense

--- app/(auth)/auth/sign-in/page.tsx
+++ app/(auth)/auth/sign-in/page.tsx
@@ -1,5 +1,4 @@
 import SignInPage from "@/components/auth/sign-in";
-import Image from "next/image";
 
 export default function SignIn() {
   return (
--- app/(auth)/auth/sign-up/page.tsx
+++ app/(auth)/auth/sign-up/page.tsx
@@ -1,5 +1,4 @@
 import SignUpPage from "@/components/auth/sign-up";
-import Image from "next/image";
 
 export default function SignUp() {
   return (
